c94e777b27579b83929aa671e450413db1ec8672
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-11 22:52:32 -0300

    Realizando buscas no banco de dados MySQL

--- src/main/java/com/bruno/eventosapp/controllers/EventoController.java
+++ src/main/java/com/bruno/eventosapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bruno.eventosapp.models.Evento;
 import com.bruno.eventosapp.repository.EventoRepository;
@@ -25,5 +26,13 @@ public class EventoController {
 		eventoRepository.save(evento);
 		
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView andView = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		andView.addObject("eventos", eventos);
+		return andView;
 	}
 }
