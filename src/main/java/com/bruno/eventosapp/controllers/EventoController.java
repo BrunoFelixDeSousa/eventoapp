28337629e313eb78bd488a63dfd963b119c9dc6c
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-16 20:14:03 -0300

    anotação @NotEmpety está descontinuada testando @Validated

--- src/main/java/com/bruno/eventosapp/controllers/EventoController.java
+++ src/main/java/com/bruno/eventosapp/controllers/EventoController.java
@@ -2,10 +2,13 @@ package com.bruno.eventosapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bruno.eventosapp.models.Convidado;
 import com.bruno.eventosapp.models.Evento;
@@ -17,23 +20,23 @@ public class EventoController {
 
 	@Autowired
 	private EventoRepository eventoRepository;
-	
+
 	@Autowired
 	private ConvidadoRepository convidadoRepository;
-	
-	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
 	}
-	
-	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
 	public String form(Evento evento) {
-		
+
 		eventoRepository.save(evento);
-		
+
 		return "redirect:/cadastrarEvento";
 	}
-	
+
 	@RequestMapping("/eventos")
 	public ModelAndView listaEventos() {
 		ModelAndView andView = new ModelAndView("index");
@@ -41,25 +44,31 @@ public class EventoController {
 		andView.addObject("eventos", eventos);
 		return andView;
 	}
-	
+
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = eventoRepository.findByCodigo(codigo);
 		ModelAndView modelAndView = new ModelAndView("evento/detalhesEvento");
 		modelAndView.addObject("evento", evento);
-		
+
 		Iterable<Convidado> convidadosIterable = convidadoRepository.findByEvento(evento);
 		modelAndView.addObject("convidados", convidadosIterable);
-		
+
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
-	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Validated Convidado convidado,
+			BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+			return "redirect:/{codigo}";
+		}
+
 		Evento evento = eventoRepository.findByCodigo(codigo);
 		convidado.setEvento(evento);
 		convidadoRepository.save(convidado);
-		
+		attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
 		return "redirect:/{codigo}";
 	}
 }
